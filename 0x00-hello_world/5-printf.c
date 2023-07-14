de135b0cabe057c88cb2f9bb1b450bf4f28acea1
kkate09 <karynmurray09@gmail.com>
2023-07-14 04:37:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("...but the outcome is a piece of art,\n");
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("...but the outcome is a piece of art,\n");
+	return (0);
 }
