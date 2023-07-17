b1c2b0fbe26811d6da1f4a27e9c7b4f385dc9c4e
kkate09 <karynmurray09@gmail.com>
2023-07-17 00:47:13 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -16,29 +16,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-
-8-print_base16.c
-
-#include <stdio.h>
-
-/**
- * main - Prints numbers between 0 to 9 and letters between a to f.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i;
-
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	for (i = 97; i < 103; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
-}
