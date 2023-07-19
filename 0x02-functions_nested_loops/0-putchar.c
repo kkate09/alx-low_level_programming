b3fb828661e67c80dfca58e1c840487c729a4feb
kkate09 <karynmurray09@gmail.com>
2023-07-19 09:17:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,16 @@
 #include "main.h"
-/**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
 
-int main(void)
+/**
+ * print_alphabet - Prints the lowercase alphabet, followed by a new line.
+ */
+void print_alphabet(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+    _putchar('\n');
 }
